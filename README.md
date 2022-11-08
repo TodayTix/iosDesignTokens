726e54c004fa8d24a5f31e35ac9642d6505a4ab9
Jessica Ingraham <jessica.ingraham@todaytix.com>
2022-11-08 12:38:26 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# iosDesignTokens
+
+A description of this package.
---
+++ Sources/iosDesignTokens/iosDesignTokens.swift
@@ -0,0 +1,6 @@
+public struct iosDesignTokens {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
