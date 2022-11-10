23ed10bfda21b66f2b14218348e5b43c169973c4
Jessica Ingraham <jessica.ingraham@todaytix.com>
2022-11-10 17:01:22 -0500

    trying to update the min version for ios

--- Package.swift
+++ Package.swift
@@ -5,16 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "iosDesignTokens",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "iosDesignTokens",
             targets: ["iosDesignTokens"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -22,8 +18,8 @@ let package = Package(
         .target(
             name: "iosDesignTokens",
             dependencies: []),
-        .testTarget(
-            name: "iosDesignTokensTests",
-            dependencies: ["iosDesignTokens"]),
+//        .testTarget(
+//            name: "iosDesignTokensTests",
+//            dependencies: ["iosDesignTokens"]),
     ]
 )
