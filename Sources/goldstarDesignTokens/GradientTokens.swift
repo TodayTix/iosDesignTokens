fbefcb4eadf5980a00af0274fcab0e51effcb51e
Jess Ingraham <jessica.ingraham@todaytix.com>
2022-11-23 17:29:53 -0500

    [sc-96168] Handle typography tokens

--- Sources/goldstarDesignTokens/GradientTokens.swift
+++ Sources/goldstarDesignTokens/GradientTokens.swift
@@ -1,151 +1,121 @@
 
 // Do not edit directly
-// Generated on Tue, 22 Nov 2022 20:44:54 GMT
+// Generated on Wed, 23 Nov 2022 22:29:52 GMT
 
 
 import UIKit
 
-public class GradientTokens {
-	public init() {}
-
-	public var lightInteractiveInteractive01: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+public extension CAGradientLayer {
+	public static func ttgLightButtonButtonPrimary() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var lightTagBrandBackground: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightMiscellaneousInteractive() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var lightTagBrandHover: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightBackgroundBackgroundBrand() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientTangerine: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.765, green: 0.137, blue: 0.137, alpha:1), UIColor(red: 1.000, green: 0.506, blue: 0.333, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightBorderBorderInteractive() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientSky: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.000, green: 0.208, blue: 0.463, alpha:1), UIColor(red: 0.114, green: 0.584, blue: 0.851, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightIconIconBrand() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientRaspberry: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.502, green: 0.024, blue: 0.220, alpha:1), UIColor(red: 0.882, green: 0.090, blue: 0.408, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightTagBrandBackground() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientGrape: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.224, green: 0.043, blue: 0.537, alpha:1), UIColor(red: 0.580, green: 0.239, blue: 0.851, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func ttgLightTagBrandHover() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientMidnight: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.157, green: 0.137, blue: 0.271, alpha:1), UIColor(red: 0.043, green: 0.043, blue: 0.063, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.95)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.05)
-			return gradientLayer
-		}
+	public static func coreGradientTangerine() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.765, green: 0.137, blue: 0.137, alpha:1), UIColor(red: 1.000, green: 0.506, blue: 0.333, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientCandyapple: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func coreGradientSky() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.000, green: 0.208, blue: 0.463, alpha:1), UIColor(red: 0.114, green: 0.584, blue: 0.851, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var coreGradientDragonfruit: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func coreGradientRaspberry() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.502, green: 0.024, blue: 0.220, alpha:1), UIColor(red: 0.882, green: 0.090, blue: 0.408, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var darkInteractiveInteractive01: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func coreGradientGrape() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.224, green: 0.043, blue: 0.537, alpha:1), UIColor(red: 0.580, green: 0.239, blue: 0.851, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var darkBackgroundUiBackground: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.157, green: 0.137, blue: 0.271, alpha:1), UIColor(red: 0.043, green: 0.043, blue: 0.063, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.95)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.05)
-			return gradientLayer
-		}
+	public static func coreGradientMidnight() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.157, green: 0.137, blue: 0.271, alpha:1), UIColor(red: 0.043, green: 0.043, blue: 0.063, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.95)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.05)
+		return gradientLayer
 	}
 
-	public var darkTagBrandBackground: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func coreGradientCandyapple() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
-	public var darkTagBrandHover: CAGradientLayer {
-		get {
-			let gradientLayer = CAGradientLayer()
-			gradientLayer.colors = [UIColor(red: 0.871, green: 0.149, blue: 0.318, alpha:1), UIColor(red: 0.882, green: 0.145, blue: 0.157, alpha:1)]
-			gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
-			gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
-			return gradientLayer
-		}
+	public static func coreGradientDragonfruit() -> CAGradientLayer {
+		let gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [UIColor(red: 1.000, green: 0.059, blue: 0.341, alpha:1), UIColor(red: 1.000, green: 0.071, blue: 0.149, alpha:1)]
+		gradientLayer.startPoint = CGPoint(x: 0.5, y: 0.05)
+		gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.95)
+		return gradientLayer
 	}
 
 }
---
+++ Sources/todaytixDesignTokens/TypographyTokens.swift
@@ -0,0 +1,607 @@
+
+// Do not edit directly
+// Generated on Wed, 23 Nov 2022 22:29:52 GMT
+
+
+import UIKit
+
+public extension UILabel {
+	static func globalH1Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 49)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH1Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 56)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH2Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 39)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH2Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 45)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH3Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 31)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH3Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 36)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH4Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 25)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH4Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 29)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH5Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 20)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH5Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "Gellix-Bold", size: 23)
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH6Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 20)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH6Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 23)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH7Sm(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 18)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalH7Md(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 21)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyPrimaryRegularSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyPrimaryRegularMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 18)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyPrimaryBoldSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyPrimaryBoldMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 18)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodySecondaryRegularSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodySecondaryRegularMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodySecondaryBoldSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodySecondaryBoldMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyTertiaryRegularSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 12)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyTertiaryRegularMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyTertiaryBoldSmall(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 12)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalBodyTertiaryBoldMedium(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalButtonPrimary(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalButtonPillRegular(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalButtonPillBold(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 14)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalTextFieldLabel(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Bold", size: 12)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalTextFieldInput(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 16)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.25
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+	static func globalTextFieldHelper(
+		with text: String,
+		configurator: (UILabel) -> Void = { _ in }
+	) -> UILabel {
+		return UILabel(multiline: true) {
+			$0.font = UIFont(name: "AktivGroteskApp-Regular", size: 12)
+			let paragraphStyle = NSMutableParagraphStyle()
+			paragraphStyle.lineHeightMultiple = 1.6
+			$0.attributedText = NSAttributedString(
+				string: text,
+				attributes: [
+					.paragraphStyle: paragraphStyle,
+					.kern: 0.01,
+				]
+			)
+			$0.textAlignment = .natural
+			configurator($0)
+		}
+	}
+
+}
