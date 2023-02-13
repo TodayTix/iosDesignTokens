6c458d55c7140285fce7781381a62e4bb65d2a0f
todaytix-engineering <44615488+todaytix-engineering@users.noreply.github.com>
2023-02-13 12:10:13 -0500

    uncomment files

--- Sources/todaytixDesignTokens/Tokens.swift
+++ Sources/todaytixDesignTokens/Tokens.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 13 Feb 2023 16:01:12 GMT
+// Generated on Mon, 13 Feb 2023 17:07:38 GMT
 
 
 import UIKit
--- Sources/todaytixDesignTokens/TypographyTokens.swift
+++ Sources/todaytixDesignTokens/TypographyTokens.swift
@@ -1,6 +1,6 @@
 
 // Do not edit directly
-// Generated on Mon, 13 Feb 2023 16:01:12 GMT
+// Generated on Mon, 13 Feb 2023 17:07:38 GMT
 
 
 import UIKit
