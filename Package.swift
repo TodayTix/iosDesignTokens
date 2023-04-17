f3a7aaf77a11b8d43597788a3c4ac5dfece8ebff
Francesca Litterio <francesca@litterio.net>
2023-04-17 10:14:05 -0400

    chore: Remove Goldstar Design Tokens [sc-112652] #5
    Remove goldstar tokens and build logic

--- Package.swift
+++ Package.swift
@@ -11,16 +11,11 @@ let package = Package(
         .library(
             name: "todaytixDesignTokens",
             targets: ["todaytixDesignTokens"]),
-        .library(
-            name: "goldstarDesignTokens",
-            targets: ["goldstarDesignTokens"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "todaytixDesignTokens",
-                dependencies: []),
-        .target(name: "goldstarDesignTokens",
                 dependencies: []),
     ]
 )
